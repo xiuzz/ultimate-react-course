561339df89c326b3a23fb1ecd117bc9485e5d67e
xiuzz <1519072696@qq.com>
2023-12-05 14:47:54 +0800

    lesson 4 final

---
+++ 04-steps/starter/step/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step >= 2)
+      setStep((st) => st-1);
+  }
+  
+  function handleNext() { 
+    if (step <= 2) 
+      setStep((st) => st+1);
+  }
+
+  function handleOpen() {
+    setOpen((op) => !op)  
+  }
+
+  return ( 
+    <> 
+      <button 
+        className="close" 
+        onClick={handleOpen}
+      >
+        &times;
+      </button>
+      {isOpen && <div className="steps"> 
+        <div className="numbers">
+          <div className={`${step >= 1? "active" : ""}`}>1</div>
+          <div className={`${step >= 2? "active" : ""}`}>2</div>
+          <div className={`${step >= 3? "active" : ""}`}>3</div>
+        </div>
+        
+        <p className="message">Step{step}:{messages[step-1]}</p>
+        
+        <div className="buttons">
+          <button 
+            style={{backgroundColor:"#7950f2", color:"#fff"}} 
+            onClick={handlePrevious}
+            >
+            Previous
+          </button>
+          <button 
+            style={{backgroundColor:"#7950f2", color:"#fff"}} 
+            onClick={handleNext}
+            >
+            Next
+          </button>
+        </div>
+      </div>
+      }
+    </>
+  )
+}
