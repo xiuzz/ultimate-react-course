d7abf4bcaefa960720cbb0feb5494c6223467e66
xiuzz <1519072696@qq.com>
2023-12-06 21:32:12 +0800

    modify 04

--- 04-steps/starter/step/src/App.js
+++ 04-steps/starter/step/src/App.js
@@ -42,21 +42,26 @@ export default function App() {
         <p className="message">Step{step}:{messages[step-1]}</p>
         
         <div className="buttons">
-          <button 
-            style={{backgroundColor:"#7950f2", color:"#fff"}} 
-            onClick={handlePrevious}
-            >
-            Previous
-          </button>
-          <button 
-            style={{backgroundColor:"#7950f2", color:"#fff"}} 
-            onClick={handleNext}
-            >
-            Next
-          </button>
+          <Button handleEvent={handlePrevious} backgroundColor="#7950f2" color="#fff">
+          👈 Previous 
+          </Button>
+          <Button handleEvent={handleNext} text="next" backgroundColor="#7950f2" color="#fff">
+          Next 👉
+          </Button>
         </div>
       </div>
       }
     </>
+  )
+}
+
+function Button({handleEvent, children, backgroundColor, color}) {
+  return(
+      <button 
+        style={{backgroundColor:backgroundColor, color:color}} 
+        onClick={handleEvent}
+      >
+        {children}
+      </button>
   )
 }
